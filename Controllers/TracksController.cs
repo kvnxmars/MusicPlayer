fada3702f350ff5f366eff427a97475068ca1633
Kevin <142409558+kvnxmars@users.noreply.github.com>
2025-06-22 19:53:18 +0200

    Comitted using git.

---
+++ Controllers/TracksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicWebAPI.Controllers
+{
+    public class TracksController : ControllerBase
+    {
+        private readonly MusicContext _context;
+
+        public TracksController(MusicContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+    //private readonly TracksController ()
+}
