0781ab5544ff5709ae1ab225d8747434ed5b5f5d
Kevin <142409558+kvnxmars@users.noreply.github.com>
2025-06-22 20:52:20 +0200

    AddTrack() + GetTrackById() added to TracksController.cs

--- Controllers/TracksController.cs
+++ Controllers/TracksController.cs
@@ -10,6 +10,38 @@ namespace MusicWebAPI.Controllers
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
+
+        public async Task<IActionResult> AddTrack([FromBody] TrackDto trackDto)
+        {
+            if (trackDto == null)
+            {
+                return BadRequest("Track data is required.");
+            }
+            var track = new Track
+            {
+                Name = trackDto.Name,
+                Artist = trackDto.Artist,
+                Album = trackDto.Album,
+                Duration = trackDto.Duration,
+                Genre = "Unknown", // Default value, can be modified later
+                ReleaseDate = DateTime.UtcNow // Set to current date and time
+            };
+            _context.Tracks.Add(track);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTrackById), new { id = track.Id }, track);
+        }
+
+        public async Task<IActionResult> GetTrackById(int id)
+        {
+            var track = await _context.Tracks.FindAsync(id);
+
+            if (track == null)
+            {
+                return NotFound("Sorry, track not found!");
+            }
+
+            return Ok(track);
+        }
     }
     //private readonly TracksController ()
 }
