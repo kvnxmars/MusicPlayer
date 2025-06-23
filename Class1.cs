fb461dd923b64a1a76eddf7dfad71ca47359c866
Kevin <142409558+kvnxmars@users.noreply.github.com>
2025-06-23 12:10:18 +0200

    First commit of the day.

--- Class1.cs
+++ Class1.cs
@@ -1,10 +1,17 @@
-﻿namespace MusicWebAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicWebAPI
 {
     public class TrackDto
     {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2–100 characters.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Artist is required.")]
         public string Artist { get; set; }
+        [Required(ErrorMessage = "Album is reuiqured.")]
         public string Album { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than zero.")]
         public int Duration { get; set; } // Duration in seconds
 
     }
